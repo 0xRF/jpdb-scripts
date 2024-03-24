9d9c808615c12dd3c8598ab6a860ca550e0b69b6
0xRF <30642254+0xRF@users.noreply.github.com>
2024-03-24 23:08:01 +1030

    upload

---
+++ menu-on.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         menu-on
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-24
+// @description  try to take over the world!
+// @author       0xRF
+// @match        https://jpdb.io/review
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=jpdb.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     document.getElementById('menu-btn').checked = true
+})();
